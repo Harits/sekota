ce17eec46abea531afabd7851d9fcef963ebd623
harits <harits@jkt1.ebdesk.com>
2026-05-21 11:01:45 +0700

    Add CORS support for Sprint 2

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation(libs.ktor.serverAuth)
     implementation(libs.ktor.serverAuthJwt)
     implementation(libs.ktor.serverContentNegotiation)
+    implementation(libs.ktor.serverCors)
     implementation(libs.ktor.serverWebsockets)
     implementation(libs.ktor.serializationKotlinxJson)
     testImplementation(libs.ktor.serverTestHost)
--- server/src/main/kotlin/com/sekota/Application.kt
+++ server/src/main/kotlin/com/sekota/Application.kt
@@ -7,6 +7,7 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
@@ -27,6 +28,11 @@ fun Application.module() {
             isLenient = true
             ignoreUnknownKeys = true
         })
+    }
+    
+    install(CORS) {
+        anyHost()
+        allowHeader(io.ktor.http.HttpHeaders.ContentType)
     }
     
     install(WebSockets) {
